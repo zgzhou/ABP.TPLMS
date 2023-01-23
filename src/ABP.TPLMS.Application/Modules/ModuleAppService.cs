7b5f2988c847bad357a429a2d58e3a75ca523fbd
zgzhou <mudage2005@163.com>
2023-01-23 18:44:34 +0800

    消除错误

--- src/ABP.TPLMS.Application/Modules/ModuleAppService.cs
+++ src/ABP.TPLMS.Application/Modules/ModuleAppService.cs
@@ -19,11 +19,12 @@ namespace ABP.TPLMS.Modules
     public class ModuleAppService : ApplicationService, IModuleAppService
     {
         private readonly IRepository<Module> _moduleRepository;
-        //AutoMapper.IMapper m_map;
+       // AutoMapper.IMapper m_map;
 
         public ModuleAppService(IRepository<Module> moduleRepository)
         {
             _moduleRepository = moduleRepository;
+        
             //m_map =map;
         }
         public Task CreateAsync(CreateUpdateModuleDto input)
